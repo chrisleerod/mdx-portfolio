e91df5c5ea46a1500870a8cfc57a73ae9e70a639
Chris Rodriguez <chrisleerod@gmail.com>
2023-01-02 16:12:52 -0600

    styling error fix (hopefully)

--- components/KeyButton/styles.index.jsx
+++ components/KeyButton/styles.index.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
-export const Container = styled(motion.Button)`
+export const Container = styled(motion.button)`
     display: flex;
     align-items: center;
 
