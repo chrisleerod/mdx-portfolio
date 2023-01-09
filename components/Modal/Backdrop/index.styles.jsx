7ecdf327abad0a48dcc12a80e77d851038b36e85
Chris Rodriguez <chrisleerod@gmail.com>
2023-01-08 18:38:32 -0600

    blur backdrop

--- components/Modal/Backdrop/index.styles.jsx
+++ components/Modal/Backdrop/index.styles.jsx
@@ -11,6 +11,7 @@ export const Container = styled(motion.div)`
     left: 0;
     height: 100%;
     width: 100%;
-    background: rgba(0, 0, 0, 0.4);
+    background: rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(2px);
     transform-origin: center;
 `;
